cdcefcb75a070e31a44e5ee777df3b1a6ada9c0c
Alethepata <alessia.patane@gmail.com>
2024-06-21 15:44:38 +0200

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import PostDetails from "./pages/PostDetails";
 
+function App() {
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path="posts" element={<Posts/>} />
+          <Route path="posts/:id" element={<PostDetails/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
